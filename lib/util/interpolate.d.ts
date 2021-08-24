f36f49c477174af5c9f568042a188fc286e7e777
root <root@localhost>
2021-08-24 21:00:18 +0200

    new concepts for this module

--- lib/util/interpolate.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const interpolate: (target: unknown, context: {
-    [key: string]: unknown;
-}, pattern?: RegExp) => unknown;
--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,34 @@
-import { series, createWithContext, Handler, Api, symbols } from 'tque';
-import { interpolate } from './util/interpolate';
+type Data = { [key: string]: unknown };
 
-type StaticApi<UserApi extends object> = {
-    registerFinalizer(fn: Function): Services<UserApi>;
-    bindArgs(fn: Function, ...args: unknown[]): Function;
-    interpolate(this: Services<UserApi>, target: unknown, source?: { [key: string]: unknown }, pattern?: RegExp): unknown;
-} & UserApi;
-
-export type Services<UserApi extends object = {}> = Api<StaticApi<UserApi>, any>;
-
-export function staticPages<UserApi extends object = {}>(userApi?: UserApi) {
-    const registeredFinalizers = new Set<Function>();
-    const services: StaticApi<UserApi> = {
-        registerFinalizer(fn) {
-            if (!registeredFinalizers.has(fn)) {
-                registeredFinalizers.add(fn);
-            }
-            return <any>this;
-        },
-
-        bindArgs(fn, ...args) {
-            return function (this: Services<UserApi>, d: unknown) {
-                args.unshift(d);
-                return fn.apply(this, args);
-            };
-        },
-
-        interpolate(target, source?, pattern?) {
-            const data = source ? source : (<any>this)[symbols.internals].data;
-            return interpolate(target, data, pattern);
-        },
-
-        ...<UserApi>userApi // undefined has no effect with spread
+export type Route = {
+    from: {
+        reader: (...args: unknown[]) => Iterable<Data> | AsyncIterable<Data>;
+        args?: unknown[];
     };
+    to: {
+        writer: (...args: unknown[]) => (data: Data) => void | Promise<void>;
+        args?: unknown[];
+    };
+    controller?: (data: Data) => undefined | Data | Data[] | Promise<undefined | Data | Data[]>;
+};
 
-    // TODO: add types for data
-    return Object.assign(async function (data: any, controllers: Handler<Services<UserApi>, any>[] = []) {
-        return await createWithContext(services, series(controllers))(data);
-    }, {
-        async finalize() {
-            for (const finalizer of registeredFinalizers) {
-                await finalizer();
+export default async (routes: Route | Route[]): Promise<void> => {
+    for (const route of Array.isArray(routes) ? routes : [routes]) {
+        const source = route.from.reader.apply(undefined, route.from.args ?? []);
+        const destination = route.to.writer.apply(undefined, route.to.args ?? []);
+        const controller = route.controller ?? (data => data);
+
+        for await (let data of source) {
+            const results = await controller(data);
+            if (results) {
+                if (Array.isArray(results)) {
+                    for (const result of results) {
+                        await destination(result);
+                    }
+                } else {
+                    await destination(results);
+                }
             }
-            registeredFinalizers.clear();
         }
-    });
-}
+    }
+};
