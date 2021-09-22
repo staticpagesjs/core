7a067865b0711fbe3b26bcee4237d5c8938edc78
root <root@localhost>
2021-09-22 20:56:47 +0200

    added error handling

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,42 @@
 export type Data = { [key: string]: unknown };
 export type Controller = (data: Data) => undefined | Data | Data[] | Promise<undefined | Data | Data[]>;
 export type Route = {
-    from: Iterable<Data> | AsyncIterable<Data>;
-    to: (data: Data) => void | Promise<void>;
-    controller?: Controller;
-    [key: string]: unknown;
+  from: Iterable<Data> | AsyncIterable<Data>;
+  to: (data: Data) => void | Promise<void>;
+  controller?: Controller;
+  [key: string]: unknown;
 };
 
-export default async (routes: Route | Route[]): Promise<void> => {
-    for (const route of Array.isArray(routes) ? routes : [routes]) {
-        const { from, to, controller, ...userOptions } = route;
-        const isController = typeof controller === "function";
+const getType = (x: any): string => typeof x === 'object' ? (x ? 'object' : 'null') : typeof x;
 
-        for await (let data of from) {
-            const results = isController ? await (controller as Controller).call(userOptions, data) : data;
-            if (typeof results === 'object' && results) {
-                if (Array.isArray(results)) {
-                    for (const result of results) {
-                        await to(result);
-                    }
-                } else {
-                    await to(results);
-                }
-            }
+export default async (routes: Route | Route[]): Promise<void> => {
+  for (const route of Array.isArray(routes) ? routes : [routes]) {
+    if (typeof route !== 'object' || !route)
+      throw new Error(`Route type mismatch, expected 'object', got '${getType(route)}'.`);
+
+    const { from, to, controller, ...userOptions } = route;
+    if (!(Symbol.iterator in from || Symbol.asyncIterator in from))
+      throw new Error(`Route 'from' is not an 'iterable' or an 'asyncIterable'.`);
+
+    if (typeof to !== 'function')
+      throw new Error(`Route 'to' type mismatch, expected 'function', got '${getType(to)}'.`);
+
+    if (typeof controller !== 'undefined' && typeof controller !== 'function')
+      throw new Error(`Route 'controller' type mismatch, expected 'function', got '${getType(controller)}'.`);
+
+    const isController = typeof controller === 'function';
+
+    for await (let data of from) {
+      const results = isController ? await (controller as Controller).call(userOptions, data) : data;
+      if (typeof results === 'object' && results) {
+        if (Array.isArray(results)) {
+          for (const result of results) {
+            await to(result);
+          }
+        } else {
+          await to(results);
         }
+      }
     }
+  }
 };
