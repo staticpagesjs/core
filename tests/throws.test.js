ea9f7d08d60eef6a72a9e8124976db9a779a51d3
root <root@localhost>
2021-10-03 18:30:34 +0200

    Update tests to 100% coverage.

--- tests/throws.test.js
+++ tests/throws.test.js
@@ -5,23 +5,23 @@ test('it should throw when the route is not an object', async () => {
     await staticPages(null);
   })
     .rejects
-    .toThrow(Error);
+    .toThrow('Route');
 });
 
 test('it shoult throw when "from" is not iterable', async () => {
   await expect(async () => {
-    await staticPages({ from: 1 });
+    await staticPages({ from: 1, to: () => undefined });
   })
     .rejects
-    .toThrow(Error);
+    .toThrow('Route');
 });
 
 test('it shoult throw when "to" is not a function', async () => {
   await expect(async () => {
-    await staticPages({ from: [], to: 1 });
+    await staticPages({ from: [], to: { a: 1 } });
   })
     .rejects
-    .toThrow(Error);
+    .toThrow('Route');
 });
 
 test('it shoult throw when "controller" is not a function', async () => {
@@ -29,5 +29,5 @@ test('it shoult throw when "controller" is not a function', async () => {
     await staticPages({ from: [], to: () => undefined, controller: 1 });
   })
     .rejects
-    .toThrow(Error);
+    .toThrow('Route');
 });
