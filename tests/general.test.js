cfa1590b05e485a5fb65d85d1bab7837d56193d0
root <root@localhost>
2022-07-10 23:32:31 +0200

    updated tests

--- tests/general.test.js
+++ tests/general.test.js
@@ -1,157 +1,135 @@
 const { Readable, Writable } = require('stream');
-const staticPages = require('../cjs/index').default;
+const { staticPages } = require('../cjs/index');
 
 const seq = n => Array.from(new Array(n)).map((v, i) => ({ a: i }));
 
 const iterableReader = function* (source) { yield* source; };
 const asyncIterableReader = async function* (source) { yield* source; };
 const streamReader = function (source) {
-    return new Readable({
-        objectMode: true,
-        read() {
-            this.push(source.shift() || null);
-        }
-    });
+	return new Readable({
+		objectMode: true,
+		read() {
+			this.push(source.shift() || null);
+		}
+	});
 };
 
-const logStreamWriter = new Writable({
-    objectMode: true,
-    write(chunk, encoding, callback) {
-        console.log(chunk);
-        callback();
-    }
+const streamWriter = new Writable({
+	objectMode: true,
+	write(chunk, encoding, callback) {
+		console.log(chunk);
+		callback();
+	}
 });
 
 test('it passes trough the input data with minimal configuration', async () => {
-    const input = seq(5);
-    const expected = seq(5);
+	const input = seq(5);
+	const expected = seq(5);
 
-    const output = [];
-    const writer = item => output.push(item);
+	const output = [];
+	const writer = item => output.push(item);
 
-    await staticPages([{
-        from: input,
-        to: writer,
-    }]);
+	await staticPages([{
+		from: input,
+		to: writer,
+	}]);
 
-    expect(output).toStrictEqual(expected);
+	expect(output).toStrictEqual(expected);
 });
 
 test('works on iterable inputs', async () => {
-    const input = iterableReader(seq(5));
-    const expected = seq(5);
+	const input = iterableReader(seq(5));
+	const expected = seq(5);
 
-    const output = [];
-    const writer = item => output.push(item);
+	const output = [];
+	const writer = item => output.push(item);
 
-    await staticPages([{
-        from: input,
-        to: writer,
-    }]);
+	await staticPages([{
+		from: input,
+		to: writer,
+	}]);
 
-    expect(output).toStrictEqual(expected);
+	expect(output).toStrictEqual(expected);
 });
 
 test('works on async iterable inputs', async () => {
-    const input = asyncIterableReader(seq(5));
-    const expected = seq(5);
+	const input = asyncIterableReader(seq(5));
+	const expected = seq(5);
 
-    const output = [];
-    const writer = item => output.push(item);
+	const output = [];
+	const writer = item => output.push(item);
 
-    await staticPages([{
-        from: input,
-        to: writer,
-    }]);
+	await staticPages([{
+		from: input,
+		to: writer,
+	}]);
 
-    expect(output).toStrictEqual(expected);
+	expect(output).toStrictEqual(expected);
 });
 
 test('works on object stream inputs', async () => {
-    const input = streamReader(seq(5));
-    const expected = seq(5);
+	const input = streamReader(seq(5));
+	const expected = seq(5);
 
-    const output = [];
-    const writer = item => output.push(item);
+	const output = [];
+	const writer = item => output.push(item);
 
-    await staticPages([{
-        from: input,
-        to: writer,
-    }]);
+	await staticPages([{
+		from: input,
+		to: writer,
+	}]);
 
-    expect(output).toStrictEqual(expected);
+	expect(output).toStrictEqual(expected);
 });
 
 test('it executes the controller which can alter the output', async () => {
-    const input = seq(5);
-    const expected = seq(5).map(x => ({ a: x.a + 1 }));
+	const input = seq(5);
+	const expected = seq(5).map(x => ({ a: x.a + 1 }));
 
-    const output = [];
-    const writer = item => output.push(item);
+	const output = [];
+	const writer = item => output.push(item);
 
-    await staticPages([{
-        from: input,
-        to: writer,
-        controller: (d) => ({ a: d.a + 1 }),
-    }]);
+	await staticPages([{
+		from: input,
+		to: writer,
+		controller: (d) => ({ a: d.a + 1 }),
+	}]);
 
-    expect(output).toStrictEqual(expected);
+	expect(output).toStrictEqual(expected);
 });
 
 test('controller can insert additional items to output', async () => {
-    const input = seq(5);
-    const expected = [];
-    for (let i = 0; i < input.length; i++) {
-        expected.push({ a: input[i].a + 1 });
-        expected.push({ b: input[i].a });
-    }
+	const input = seq(5);
+	const expected = [];
+	for (let i = 0; i < input.length; i++) {
+		expected.push({ a: input[i].a + 1 });
+		expected.push({ b: input[i].a });
+	}
 
-    const output = [];
-    const writer = item => output.push(item);
+	const output = [];
+	const writer = item => output.push(item);
 
-    await staticPages([{
-        from: input,
-        to: writer,
-        controller: (d) => [{ a: d.a + 1 }, { b: d.a }], // output two items for each input item
-    }]);
+	await staticPages([{
+		from: input,
+		to: writer,
+		controller: (d) => [{ a: d.a + 1 }, { b: d.a }], // output two items for each input item
+	}]);
 
-    expect(output).toStrictEqual(expected);
+	expect(output).toStrictEqual(expected);
 });
 
 test('controller can remove items from output', async () => {
-    const input = seq(5);
-    const expected = seq(5).filter(x => x.a % 2 === 0);
+	const input = seq(5);
+	const expected = seq(5).filter(x => x.a % 2 === 0);
 
-    const output = [];
-    const writer = item => output.push(item);
+	const output = [];
+	const writer = item => output.push(item);
 
-    await staticPages([{
-        from: input,
-        to: writer,
-        controller: (d) => d.a % 2 === 0 ? d : undefined, // mod2? d / nothing
-    }]);
+	await staticPages([{
+		from: input,
+		to: writer,
+		controller: (d) => d.a % 2 === 0 ? d : undefined, // mod2? d / nothing
+	}]);
 
-    expect(output).toStrictEqual(expected);
-});
-
-test('it passes variables to the controller as `this`', async () => {
-    const MY_TEXT = "this is my custom config.";
-    const input = seq(5);
-    const expected = seq(5).map(x => ({ ...x, m: MY_TEXT }));
-
-    const output = [];
-    const writer = item => output.push(item);
-
-    await staticPages([{
-        from: input,
-        to: writer,
-        controller: function (data) {
-            return { ...data, m: this.myConfigOption };
-        },
-        variables: {
-            myConfigOption: MY_TEXT,
-        },
-    }]);
-
-    expect(output).toStrictEqual(expected);
+	expect(output).toStrictEqual(expected);
 });
--- tests/throws.test.js
+++ tests/throws.test.js
@@ -1,41 +1,41 @@
-const staticPages = require('../cjs/index').default;
+const { staticPages } = require('../cjs/index');
 
 test('it should throw when the route is not an object', async () => {
-  await expect(async () => {
-    await staticPages(null);
-  })
-    .rejects
-    .toThrow('Route');
+	await expect(async () => {
+		await staticPages(null);
+	})
+		.rejects
+		.toThrow('Route');
 });
 
 test('it shoult throw when "from" is not iterable', async () => {
-  await expect(async () => {
-    await staticPages({ from: 1, to: () => undefined });
-  })
-    .rejects
-    .toThrow('Route');
+	await expect(async () => {
+		await staticPages({ from: 1, to: () => undefined });
+	})
+		.rejects
+		.toThrow('Route');
 });
 
 test('it shoult throw when "to" is not a function', async () => {
-  await expect(async () => {
-    await staticPages({ from: [], to: { a: 1 } });
-  })
-    .rejects
-    .toThrow('Route');
+	await expect(async () => {
+		await staticPages({ from: [], to: { a: 1 } });
+	})
+		.rejects
+		.toThrow('Route');
 });
 
 test('it shoult throw when "controller" is not a function', async () => {
-  await expect(async () => {
-    await staticPages({ from: [], to: () => undefined, controller: 1 });
-  })
-    .rejects
-    .toThrow('Route');
+	await expect(async () => {
+		await staticPages({ from: [], to: () => undefined, controller: 1 });
+	})
+		.rejects
+		.toThrow('Route');
 });
 
 test('it shoult throw when from and to is undefined', async () => {
-  await expect(async () => {
-    await staticPages({ from: undefined, to: undefined });
-  })
-    .rejects
-    .toThrow('Route');
+	await expect(async () => {
+		await staticPages({ from: undefined, to: undefined });
+	})
+		.rejects
+		.toThrow('Route');
 });
