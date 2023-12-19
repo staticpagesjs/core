238607bcbd423f01f19e8aa86a9fcbecd5c61d2c
root <root@localhost>
2023-12-19 23:39:15 +0100

    improve tests

--- test/create-reader.test.js
+++ test/create-reader.test.js
@@ -1,7 +1,7 @@
 import assert from 'assert';
 import { createReader } from '../esm/index.js';
 
-const minimalBackend = {
+const mockBackend = {
 	tree(){ return ['1', '2']; },
 	read(f){ return { '1': '"one"', '2': '"two"' }[f]; },
 	write(){}
@@ -13,7 +13,7 @@ describe('Static Pages CreateReader Tests', () => {
 		const recieved = [];
 
 		const reader = createReader({
-			backend: minimalBackend,
+			backend: mockBackend,
 		});
 
 		for await (const item of reader) {
@@ -28,7 +28,7 @@ describe('Static Pages CreateReader Tests', () => {
 		const recieved = [];
 
 		const reader = createReader({
-			backend: minimalBackend,
+			backend: mockBackend,
 			pattern: '2'
 		});
 
@@ -44,8 +44,26 @@ describe('Static Pages CreateReader Tests', () => {
 		const recieved = [];
 
 		const reader = createReader({
-			backend: minimalBackend,
+			backend: mockBackend,
 			ignore: '1'
+		});
+
+		for await (const item of reader) {
+			recieved.push(item);
+		}
+
+		assert.deepStrictEqual(recieved, expected);
+	});
+
+	it('calls finally when iteration done', async () => {
+		const expected = ['one', 'two', 'finally'];
+		const recieved = [];
+
+		const reader = createReader({
+			backend: mockBackend,
+			finally() {
+				recieved.push('finally');
+			}
 		});
 
 		for await (const item of reader) {
@@ -60,7 +78,7 @@ describe('Static Pages CreateReader Tests', () => {
 		let recieved = null;
 
 		const reader = createReader({
-			backend: minimalBackend,
+			backend: mockBackend,
 			parse() { throw new Error('Some error thrown.'); },
 			catch(error) {
 				recieved = error.message;
@@ -75,31 +93,13 @@ describe('Static Pages CreateReader Tests', () => {
 	it('should throw with default settings on reading/parsing errors', async () => {
 		await assert.rejects(async () => {
 			const reader = createReader({
-				backend: minimalBackend,
+				backend: mockBackend,
 				parse() { throw new Error('Some error thrown.'); },
 			});
 
 			await reader.next();
 
 		}, { message: `Some error thrown.` });
-	});
-
-	it('calls finally when iteration done', async () => {
-		const expected = ['one', 'two', 'finally'];
-		const recieved = [];
-
-		const reader = createReader({
-			backend: minimalBackend,
-			finally() {
-				recieved.push('finally');
-			}
-		});
-
-		for await (const item of reader) {
-			recieved.push(item);
-		}
-
-		assert.deepStrictEqual(recieved, expected);
 	});
 
 	it('should throw when "backend.tree" returns non iterable', async () => {
@@ -116,4 +116,100 @@ describe('Static Pages CreateReader Tests', () => {
 
 		}, { message: `Expected 'Iterable' or 'AsyncIterable' at 'backend.tree()' call.` });
 	});
+
+	it('should throw when "backend" recieves an invalid type', async () => {
+		await assert.rejects(async () => {
+			const reader = createReader({
+				backend: 1,
+			});
+
+			await reader.next();
+
+		}, { message: `Expected 'Backend' implementation at 'backend' property.` });
+	});
+
+	it('should throw when "cwd" recieves an empty string', async () => {
+		await assert.rejects(async () => {
+			const reader = createReader({
+				backend: mockBackend,
+				cwd: ''
+			});
+
+			await reader.next();
+
+		}, { message: `Expected non-empty string at 'cwd'.` });
+	});
+
+	it('should throw when "cwd" recieves a non string value', async () => {
+		await assert.rejects(async () => {
+			const reader = createReader({
+				backend: mockBackend,
+				cwd: 123
+			});
+
+			await reader.next();
+
+		}, { message: `Expected 'string', recieved 'number' at 'cwd' property.` });
+	});
+
+	it('should throw when "pattern" recieves a non string and non array value', async () => {
+		await assert.rejects(async () => {
+			const reader = createReader({
+				backend: mockBackend,
+				pattern: 123
+			});
+
+			await reader.next();
+
+		}, { message: `Expected 'string' or 'string[]', recieved 'number' at 'pattern' property.` });
+	});
+
+	it('should throw when "ignore" recieves a non string and non array value', async () => {
+		await assert.rejects(async () => {
+			const reader = createReader({
+				backend: mockBackend,
+				ignore: 123
+			});
+
+			await reader.next();
+
+		}, { message: `Expected 'string' or 'string[]', recieved 'number' at 'ignore' property.` });
+	});
+
+	it('should throw when "parse" recieves a non callable', async () => {
+		await assert.rejects(async () => {
+			const reader = createReader({
+				backend: mockBackend,
+				parse: 123
+			});
+
+			await reader.next();
+
+		}, { message: `Expected 'function', recieved 'number' at 'parse' property.` });
+	});
+
+	it('should throw when "catch" recieves a non callable', async () => {
+		await assert.rejects(async () => {
+			const reader = createReader({
+				backend: mockBackend,
+				catch: 123
+			});
+
+			await reader.next();
+
+		}, { message: `Expected 'function', recieved 'number' at 'catch' property.` });
+	});
+
+	it('should throw when "finally" recieves a non callable', async () => {
+		await assert.rejects(async () => {
+			const reader = createReader({
+				backend: mockBackend,
+				finally: 123
+			});
+
+			await reader.next();
+
+		}, { message: `Expected 'function', recieved 'number' at 'finally' property.` });
+	});
+
 });
--- test/throws.test.js
+++ test/throws.test.js
@@ -12,7 +12,7 @@ describe('Static Pages Throws Tests', () => {
 
 	it('should throw when "from" is not iterable', async () => {
 		await assert.rejects(async () => {
-			await staticPages({ from: 1, to: () => undefined });
+			await staticPages({ from: { [Symbol.asyncIterator]: 1 }, to: () => undefined });
 		}, { message: `Expected 'Iterable' or 'AsyncIterable' at 'from' property.` });
 	});
 
@@ -28,127 +28,10 @@ describe('Static Pages Throws Tests', () => {
 		}, { message: `Expected 'function', recieved 'number' at 'controller' property.` });
 	});
 
-	it('should throw when "from" and to are undefined', async () => {
+	it('should throw when "from" and "to" are undefined', async () => {
 		await assert.rejects(async () => {
 			await staticPages({ from: undefined, to: undefined });
 		}, { message: `Expected 'Iterable' or 'AsyncIterable' at 'from' property.` });
-	});
-
-	it('should throw when "from.backend" recieves an invalid type', async () => {
-		await assert.rejects(async () => {
-			await staticPages({
-				from: { backend: 1 },
-				to: {
-					backend: mockBackend,
-					render: JSON.stringify
-				}
-			});
-		}, { message: `Expected 'Backend' implementation at 'backend' property.` });
-	});
-
-	it('should throw when "from.cwd" recieves an empty string', async () => {
-		await assert.rejects(async () => {
-			await staticPages({
-				from: {
-					backend: mockBackend,
-					cwd: ''
-				},
-				to: {
-					backend: mockBackend,
-					render: JSON.stringify
-				}
-			});
-		}, { message: `Expected non-empty string at 'cwd'.` });
-	});
-
-	it('should throw when "from.cwd" recieves a non string value', async () => {
-		await assert.rejects(async () => {
-			await staticPages({
-				from: {
-					backend: mockBackend,
-					cwd: 123
-				},
-				to: {
-					backend: mockBackend,
-					render: JSON.stringify
-				}
-			});
-		}, { message: `Expected 'string', recieved 'number' at 'cwd' property.` });
-	});
-
-	it('should throw when "from.pattern" recieves a non string and non array value', async () => {
-		await assert.rejects(async () => {
-			await staticPages({
-				from: {
-					backend: mockBackend,
-					pattern: 123
-				},
-				to: {
-					backend: mockBackend,
-					render: JSON.stringify
-				}
-			});
-		}, { message: `Expected 'string' or 'string[]', recieved 'number' at 'pattern' property.` });
-	});
-
-	it('should throw when "from.ignore" recieves a non string and non array value', async () => {
-		await assert.rejects(async () => {
-			await staticPages({
-				from: {
-					backend: mockBackend,
-					ignore: 123
-				},
-				to: {
-					backend: mockBackend,
-					render: JSON.stringify
-				}
-			});
-		}, { message: `Expected 'string' or 'string[]', recieved 'number' at 'ignore' property.` });
-	});
-
-	it('should throw when "from.parse" recieves a non callable', async () => {
-		await assert.rejects(async () => {
-			await staticPages({
-				from: {
-					backend: mockBackend,
-					parse: 123
-				},
-				to: {
-					backend: mockBackend,
-					render: JSON.stringify
-				}
-			});
-		}, { message: `Expected 'function', recieved 'number' at 'parse' property.` });
-	});
-
-	it('should throw when "from.catch" recieves a non callable', async () => {
-		await assert.rejects(async () => {
-			await staticPages({
-				from: {
-					backend: mockBackend,
-					catch: 123
-				},
-				to: {
-					backend: mockBackend,
-					render: JSON.stringify
-				}
-			});
-		}, { message: `Expected 'function', recieved 'number' at 'catch' property.` });
-	});
-
-	it('should throw when "from.finally" recieves a non callable', async () => {
-		await assert.rejects(async () => {
-			await staticPages({
-				from: {
-					backend: mockBackend,
-					finally: 123
-				},
-				to: {
-					backend: mockBackend,
-					render: JSON.stringify
-				}
-			});
-		}, { message: `Expected 'function', recieved 'number' at 'finally' property.` });
 	});
 
 	it('should throw when "to.backend" recieves an an invalid type', async () => {
